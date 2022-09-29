3a362bb7c242eea40f387c0d65169cebc92a37c3
Alvaro R Martin <melkor@aleph.engineering>
2022-09-28 23:34:42 -0400

    Add Media to the game
    Adding sounds and materials to the game

---
+++ Assets/Scripts/EffectsManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectsManager : MonoBehaviour
+{
+    public GameObject _lava_spark_effect;
+    public GameObject _lava_river;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

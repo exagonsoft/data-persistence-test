58bcf3f170f3480130c31e4bf2cb9fc602a4f04d
Alvaro R Martin <melkor@aleph.engineering>
2022-09-30 14:18:49 -0400

    Implement Buttons Actions
    Implementing the function of the buttons handler

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void OnExit() {
+        #if UNITY_EDITOR
+           EditorApplication.ExitPlaymode();
+        #else
+           Application.Quit(); // original code to quit Unity player
+        #endif
+    }
+
+    public void OnStartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnSettings()
+    {
+
+    }
+
+    public void OnHigthScores()
+    {
+
+    }
+}
